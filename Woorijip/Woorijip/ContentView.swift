81361546b265929808e908188d369bbb711fd742
kimjjunho <kimjjun1007@gmail.com>
2023-04-05 12:04:43 +0900

    start

---
+++ Woorijip/Woorijip/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Woorijip
+//
+//  Created by 김준호 on 2023/04/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Woorijip/Woorijip/WoorijipApp.swift
@@ -0,0 +1,17 @@
+//
+//  WoorijipApp.swift
+//  Woorijip
+//
+//  Created by 김준호 on 2023/04/05.
+//
+
+import SwiftUI
+
+@main
+struct WoorijipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
