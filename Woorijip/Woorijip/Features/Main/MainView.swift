3c98e53aeef6a61e9d292b5bee11e3cc4028e955
kimjjunho <kimjjun1007@gmail.com>
2023-06-14 12:28:02 +0900

    lazy vstack

--- Woorijip/Woorijip/Features/Main/MainView.swift
+++ Woorijip/Woorijip/Features/Main/MainView.swift
@@ -5,13 +5,24 @@ struct MainView: View {
     @State private var isSearchNavigate = false
     @StateObject var viewModel = MainViewModel()
     
+    private let items = (1...50).map { "Item \($0)" }
+    
     let icons = [
         "Home",
         "Plus",
         "Mypage",
     ]
     
+    let columns: [GridItem] = [
+        GridItem(.flexible(), spacing: nil, alignment: nil),
+        GridItem(.flexible(), spacing: nil, alignment: nil)
+    ]
+    
+    var itemData: [String] = [""]
+    
+    
     var body: some View {
+        
         VStack {
             ZStack {
                 switch selectedIndex {
@@ -20,35 +31,83 @@ struct MainView: View {
                         VStack() {
                             Spacer().frame(height: 33)
                             HStack {
-                                Text("우리동네").wooriFont(font: .AppleSDGothicNeoBold, size: 18)
+                                Text("대전광역시 유성구 장동").wooriFont(font: .AppleSDGothicNeoBold, size: 18)
                                 Spacer()
                                 Button(action: { isSearchNavigate.toggle() }) {
                                     Image("Search")
                                 }
                             }
-                            Spacer()
-                            Image("Empty")
-                            Spacer().frame(height: 40.5)
-                            Text("우리 동네에 모임이 없어요.")
-                                .wooriFont(font: .NanumMyeongjo, size: 14)
-                            Spacer()
-                            WooriButton(text: "모임 만들기") {
-                                
+
+                            if(itemData.isEmpty) {
+                                Spacer()
+                                Image("Empty")
+                                Spacer().frame(height: 40.5)
+                                Text("우리 동네에 모임이 없어요.")
+                                    .wooriFont(font: .NanumMyeongjo, size: 14)
+                                Spacer()
+                                WooriButton(text: "모임 만들기") {
+
+                                }
+                                Spacer().frame(height: 30)
+                            } else {
+                                Spacer().frame(height: 30)
+                                ScrollView {
+                                    LazyVGrid(
+                                        columns: columns,
+                                        alignment: .center,
+                                        spacing: nil,
+                                        pinnedViews: [],
+                                        content: {
+                                            ForEach(0..<50) { index in
+                                                VStack(alignment: .leading) {
+                                                    ZStack(alignment: .topLeading) {
+                                                        Image("Null").frame(height: 100)
+                                                        Text("42회 진행")
+                                                            .wooriFont(font: .AppleSDGothicNeoRegular, size: 10)
+                                                            .foregroundColor(.white)
+                                                            .padding(.horizontal, 8)
+                                                            .padding(.vertical, 2)
+                                                            .background(Color("MainColor"))
+                                                            .padding(.leading, 4)
+                                                            .padding(.top, 8)
+                                                    }
+                                                    
+                                                    Spacer().frame(height: 4)
+                                                    
+                                                    Text("제목 폼")
+                                                        .wooriFont(font: .NanumMyeongjoBold, size: 12)
+                                                    Text("오늘의 집 4회 소개! 색감맛집")
+                                                        .wooriFont(font: .NanumMyeongjo, size: 10)
+                                                    
+                                                    Spacer().frame(height: 8)
+                                                    
+                                                    Text("서울특별시 관악구 봉천동")
+                                                        .foregroundColor(Color("Gray400"))
+                                                        .wooriFont(font: .NanumMyeongjo, size: 10)
+                                                    
+                                                    Spacer()
+                                                }
+                                                .frame(height: 190)
+                                            }
+                                        }
+                                    )
+                                }
+                                Spacer()
                             }
-                            Spacer().frame(height: 30)
+
                         }.padding(.horizontal, 40)
                     }
                 case 1:
                     NavigationView {
-                        
+
                     }
                 default:
                     NavigationView {
-                        
+
                     }
                 }
-                
-                
+
+
             }
             .navigationBarHidden(true)
             .navigate(to: SearchView(), when: $isSearchNavigate)
@@ -70,7 +129,7 @@ struct MainView: View {
                                     .wooriFont(font: .AppleSDGothicNeoRegular, size: 10)
                                     .foregroundColor(selectedIndex == number ? Color("Gray900") : Color("Gray300"))
                             }
-                            
+
                         })
                         Spacer()
                     } else {
@@ -80,7 +139,7 @@ struct MainView: View {
                         })
                         Spacer()
                     }
-                   
+
                 }
                 .onAppear {
                     viewModel.onAppear()
--- Woorijip/Woorijip/WoorijipApp.swift
+++ Woorijip/Woorijip/WoorijipApp.swift
@@ -4,8 +4,7 @@ import SwiftUI
 struct WoorijipApp: App {
     var body: some Scene {
         WindowGroup {
-        OnboardView()
-
+            MainView()
         }
     }
 }
